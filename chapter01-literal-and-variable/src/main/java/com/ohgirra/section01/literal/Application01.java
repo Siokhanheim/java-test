68a0d418aa98b0d05fb943bc88dc56a7d165e266
sunghwan <sunghwan941027@gmail.com>
2024-04-11 18:01:46 +0900

    first commit

---
+++ chapter01-literal-and-variable/src/main/java/com/ohgirra/section01/literal/Application01.java
@@ -0,0 +1,26 @@
+package com.ohgirra.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        // 정수형 리터럴
+        System.out.println(123);
+        // 실수형 리터럴
+        System.out.println(1.23);
+            System.out.println("Hello World");
+            // 문자형 리터럴
+            System.out.println('A');
+            // 문자열형 리터럴
+            System.out.println("Hello world");
+
+            System.out.println("12345");
+            System.out.print("123.45");
+
+            //논리형 리터럴
+            System.out.println(true);
+            System.out.println(false);
+
+    }
+}
+
